1caf7e4fc069123d27a7fce9e70dbb18c07cb92f
Ares42 <Luke Solomon>
2014-06-25 13:18:56 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,25 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Luke Solomon on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+
+-(id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+
+@end
