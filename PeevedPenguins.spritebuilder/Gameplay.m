a4d38266f448a47fb3d73a5cd496c3807cc86c72
Ares42 <Luke Solomon>
2014-06-27 10:06:30 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -154,14 +154,17 @@
 
 - (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
     
-    CCLOG(@"Something collided with a seal!");
+    
     
     float energy = [pair totalKineticEnergy];
     
     // if energy is large enough, remove the seal
     if (energy > 5000.f) {
         [[_physicsNode space] addPostStepBlock:^{
-            [self sealRemoved:nodeA];
+            
+           // [self sealRemoved:nodeA];
+            CCLOG(@"Something collided with a seal!");
+            
         } key:nodeA];
     }
     
